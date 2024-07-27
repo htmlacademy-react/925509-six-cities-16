8d7b59fb8cb4aefe81fa1849abd12a9fc7f7a2c2
ZinchAndrey <zinchandrey@gmail.com>
2024-07-27 16:13:08 +0300

    Добавляет компонент футера

--- src/pages/favorites/favorites-page.tsx
+++ src/pages/favorites/favorites-page.tsx
@@ -1,4 +1,5 @@
 import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
 import FavoritesList from '../../components/favorites/favorites-locations-list';
 
 import { placesList } from '../../mocks/mocks';
@@ -18,17 +19,7 @@ function FavoritesPage(): JSX.Element {
           </section>
         </div>
       </main>
-      <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img
-            className="footer__logo"
-            src="img/logo.svg"
-            alt="6 cities logo"
-            width={64}
-            height={33}
-          />
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
